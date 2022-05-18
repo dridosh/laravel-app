82e52eb40690b63c4b8b04f57fe7222d5c6e5946
D.Ridosh <dridosh@gmail.com>
2022-05-18 13:27:04 +0400

    create articles catalog #16

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,46 +1,50 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Str;
 
-class Article extends Model
-{
-    use HasFactory;
+    class Article extends Model {
+        use HasFactory;
 
-    protected $dates = ['published_at'];
+        protected $dates = ['published_at'];
 
-    protected $fillable = ['title', 'body', 'img', 'slug'];
+        protected $fillable = ['title', 'body', 'img', 'slug'];
 
-    public function comments () {
-        return $this->hasMany(Comment::class);
+        public function comments () {
+            return $this->hasMany(Comment::class);
+        }
+
+        public function state () {
+            return $this->hasOne(State::class);
+        }
+
+        public function tags () {
+            return $this->belongsToMany(Tag::class);
+
+        }
+
+        public function getBodyPreview () {
+            return Str::limit($this->body, 100);
+        }
+
+        public function createdAtForHumans () {
+            return $this->created_at->diffForHumans();
+        }
+
+        public function publishedAtForHumans () {
+            return $this->published_at->diffForHumans();
+        }
+
+        public function scopeLastLimit ($query, $numbers) {
+            return $query->with('state', 'tags')->orderBy('created_at', 'desc')->take($numbers)->get();
+        }
+
+        public function scopeAllPaginate ($query, $number) {
+            return $query->with('state', 'tags')->orderBy('created_at', 'desc')->paginate($number);
+
+        }
+
     }
-
-    public function state () {
-        return $this->hasOne(State::class);
-    }
-
-    public function tags () {
-        return $this->belongsToMany(Tag::class);
-
-    }
-
-    public function getBodyPreview () {
-        return Str::limit($this->body, 100);
-    }
-
-    public function createdAtForHumans () {
-        return $this->created_at->diffForHumans();
-    }
-
-    public function publishedAtForHumans () {
-        return $this->published_at->diffForHumans();
-    }
-
-    public function scopeLastLimit ($query,$numbers) {
-        return $query->with('state','tags')->orderBy('created_at', 'desc')->take($numbers) ->get() ;
-    }
-
-}
