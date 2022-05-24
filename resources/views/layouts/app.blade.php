734845ec71c682b8ff6375de75f8a85b31f7c134
D.Ridosh <dridosh@gmail.com>
2022-05-24 18:26:07 +0400

    create active button on navbar #20

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,11 +21,12 @@
                 <div class="container-fluid">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link menu-link  "
+                            <a class="nav-link {{ request() -> is('/') ? 'menu-link__active' : 'menu-link'}}"
                                href="{{route('home')}}">Главная страница</a>
+
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link menu-link "
+                            <a class="nav-link {{ request() -> is('articles') ? 'menu-link__active' : 'menu-link'}} "
                                href="{{route('article.index')}}">Каталог статей</a>
                         </li>
                     </ul>
